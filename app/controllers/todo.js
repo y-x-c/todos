1d7483322efdb9dad073d74a3eff08f408444c7f
Orthocenter <chenyuxin.mail@gmail.com>
2015-06-12 17:58:42 +0800

    support both insert-newline and focus-out && turn isEditing into controller attr

--- app/controllers/todo.js
+++ app/controllers/todo.js
@@ -3,19 +3,19 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   actions: {
     editTodo: function() {
-      this.set('model.isEditing', true);
+      this.set('isEditing', true);
     },
     saveChanges: function() {
       if(this.get('model.title').trim().length) {
         this.model.save();
-        this.set('model.isEditing', false);
+        this.set('isEditing', false);
       } else {
         this.send('removeTodo');
       }
     },
     removeTodo: function() {
       this.model.destroyRecord();
-      this.set('model.isEditing', false);
+      this.set('isEditing', false);
     }
   }
 });
