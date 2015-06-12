c6a368a5bb7eecef809b57bbca9e2e620d8acd3d
Orthocenter <chenyuxin.mail@gmail.com>
2015-06-12 19:02:32 +0800

    link style

--- app/routes/todos/completed.js
+++ app/routes/todos/completed.js
@@ -3,7 +3,6 @@ import Ember from 'ember';
 export default Ember.Route.extend({
     model: function() {
         var todos = this.store.find('todo');
-        //return todos.filterBy('isCompleted', true);
         return this.store.filter('todo', function(todo) {
           return todo.get('isCompleted');
         });
